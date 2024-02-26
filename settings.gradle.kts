d236c3b0684721970c3eb76116815a339ce0d3d7
Adrien Tétar <adri-from-59@hotmail.fr>
2024-02-26 13:53:43 -0800

    Update dependencies and add run configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,14 @@
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("kotlin", "1.8.21")
+            version("kotlin", "1.9.22")
             version("library", "0.1.0")
-            version("plist", "1.26")
-            version("xmlutil", "0.85.0")
+            version("plist", "1.27")
+            version("xmlutil", "0.86.3")
 
             version("jacoco", "0.8.10")
             version("jimfs", "1.2")
-            version("truth", "1.1.3")
+            version("truth", "1.4.1")
         }
     }
 }
